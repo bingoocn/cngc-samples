8bdcdf0c8f7a6e3f0b522d95b367371f29516a0b
duanyinglong <kylerduane@163.com>
2019-08-02 09:41:01 +0800

    手动进行请求体数据校验,抛出异常

--- boot-sample-validation/src/main/java/sample/validation/web/SampleController.java
+++ boot-sample-validation/src/main/java/sample/validation/web/SampleController.java
@@ -1,11 +1,14 @@
 package sample.validation.web;
 
+import com.cngc.boot.web.constant.RequestBodyErrorCode;
 import com.cngc.boot.web.exception.RequestBodyValidationException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import sample.validation.entity.SampleData;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 
 /**
  * 测试用controller.
@@ -16,9 +19,17 @@ import javax.validation.Valid;
 @RequestMapping("sample")
 public class SampleController {
 
-    @PostMapping
-    public SampleData createSample(@Valid @RequestBody SampleData sampleData) {
+    @PutMapping
+    public SampleData updateSample(@Valid @RequestBody SampleData sampleData) {
         return sampleData;
+    }
+
+    @PostMapping
+    public SampleData createSample(@Valid @RequestBody SampleData sampleData, BindingResult bindingResult) throws RequestBodyValidationException {
+        // 手动进行请求体数据校验,抛出异常.
+        FieldError fieldError = new FieldError("", "id", null, false,
+                new String[]{RequestBodyErrorCode.ALREADY_EXISTS}, null, "新增资源已存在");
+        throw new RequestBodyValidationException(Arrays.asList(new FieldError[]{fieldError}));
     }
 
 
