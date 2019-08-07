f23ab4b473bbef8d027e26755e2b97a79dbd6aa6
kylerduane <kylerduane@163.com>
2019-08-07 22:47:08 +0800

    temp

--- boot-sample-log/src/main/java/com/cngc/boot/web/log/RequestLogInfo.java
+++ boot-sample-log/src/main/java/com/cngc/boot/web/log/RequestLogInfo.java
@@ -1,10 +1,24 @@
 package com.cngc.boot.web.log;
 
+import lombok.Data;
+
+import java.util.Date;
+
 /**
  * 请求日志类.
  *
  * @author maxD
  */
+@Data
 public class RequestLogInfo {
 
+    private String loginAccount;
+    private String userName;
+    private String orgName;
+    private String ip;
+    private String uri;
+    private String hostUrl;
+    private Date requestTime;
+    private String message;
+    private String request;
 }
