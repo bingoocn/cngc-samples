d38b31db6a7e26b30cfefd2392a75a3e8aa7a431
duanyinglong <kylerduane@163.com>
2019-11-20 15:43:36 +0800

    增加日志功能

--- boot-sample-log/src/main/java/sample/log/Application.java
+++ boot-sample-log/src/main/java/sample/log/Application.java
@@ -7,6 +7,7 @@ package sample.log;
 import com.cngc.boot.core.annotation.EnableCngcBoot;
 import com.cngc.boot.integration.r1.portal.annotation.EnableR1IntegrationAnnotation;
 import com.cngc.boot.web.annotation.EnableCngcWebMvc;
+import com.cngc.boot.web.annotation.EnableRequestLog;
 import com.cngc.boot.web.dictionary.translate.EnableDictTranslate;
 import com.cngc.boot.web.log.RequestLogAspect;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +19,7 @@ import org.springframework.context.annotation.Import;
 @EnableCngcBoot
 @EnableCngcWebMvc
 @EnableDictTranslate
-@Import(RequestLogAspect.class)
+@EnableRequestLog
 public class Application {
 
     public static void main(String[] args) {
